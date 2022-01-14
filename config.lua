42a8395b712c8b894194f4ec7406d3a4f8934373
Francesco Metta <fra.metta@hotmail.it>
2022-01-14 15:30:40 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,40 @@
+Config = {}
+
+
+--[[
+    language flag
+
+    available languages: it, en
+]]
+Config.Locale = 'it'
+
+--[[
+    esx menu alignment
+
+    available flags: 
+        bottom, 
+        top,
+        center
+]]
+Config.menuAlign = 'bottom-right'
+
+--[[
+    Notification system
+
+    Set true if you're using mythic_notify by thelindat -> https://github.com/thelindat/mythic_notify
+    If false uses the default notification system.
+]]
+Config.usingMythic = true
+
+--[[
+    Default command name
+]]
+Config.defaultCommand = 'sim'
+
+--[[
+    Keymap system
+
+    Default key (F3).
+]]
+Config.usingKeymap = true
+Config.customKey = 'F3'
