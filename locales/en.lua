d0886689d2c7171869dbc09d800fac2bc9c5d9ef
Francesco Metta <fra.metta@hotmail.it>
2022-01-14 15:30:27 +0100

    lang files

---
+++ locales/en.lua
@@ -0,0 +1,18 @@
+Locales['it'] = {
+    --client side
+    ['use'] = 'Use',
+    ['give'] = 'Give',
+    ['throw'] = 'Throw',
+    ['simMenu'] = 'SIM Menu',
+    ['simOptions'] = 'SIM Options',
+    ['noPhone'] = 'You don\'t have a phone.',
+    ['noPlayerNear'] = 'No players nearby',
+    ['keyMapdesc'] = 'SIM menu key',
+
+    --server side
+    ['newSim'] = 'You have activated a new SIM. Phone number: %s',
+    ['switchedSim'] = 'You have installed the SIM number: %s',
+    ['throwedSim'] = 'You threw away the SIM number: %s',
+    ['gaveAway'] = 'You gave away the SIM number: %s',
+    ['receivedSim'] = 'You have received the SIM number: %s',
+}
---
+++ locales/it.lua
@@ -0,0 +1,18 @@
+Locales['it'] = {
+    --client side
+    ['use'] = 'Usa',
+    ['give'] = 'Dai',
+    ['throw'] = 'Butta',
+    ['simMenu'] = 'Menu delle SIM',
+    ['simOptions'] = 'Operazioni SIM',
+    ['noPhone'] = 'Non hai un telefono.',
+    ['noPlayerNear'] = 'Nessun giocatore nelle vicinanze',
+    ['keyMapdesc'] = 'Tasto menu SIM',
+
+    --server side
+    ['newSim'] = 'Hai attivato una nuova SIM. Numero: %s',
+    ['switchedSim'] = 'Hai inserito la SIM numero: %s',
+    ['throwedSim'] = 'Hai gettato via la SIM numero: %s',
+    ['gaveAway'] = 'Hai dato via la SIM numero: %s',
+    ['receivedSim'] = 'Hai ricevuto la SIM numero: %s',
+}
